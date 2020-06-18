264652d6908d61598dffe6bb7f81a654a5608c0f
C4Jay <choolakejay@gmail.com>
2020-06-18 20:32:03 +0530

    style

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import About from './About';
 import styles from './Home.module.css';
+import TextField from '@material-ui/core/TextField';
 
 class Home extends Component {
   render() {
@@ -10,6 +11,10 @@ class Home extends Component {
         <div className={styles['title']}>
           <h2>Home</h2>
           <Link to="/about" style={{color : 'white'}}>about</Link>
+          <div className={styles['field']}>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          </div>
+
         </div>
     );
   }
