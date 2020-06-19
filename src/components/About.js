6e68cf5ac8101cf8290034e693f8da9b1b8d6f06
C4Jay <choolakejay@gmail.com>
2020-06-19 10:23:45 +0530

    cs

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,16 @@ class About extends Component {
   render() {
     return (
         <div className={styles.main}>
+          <div className={styles.title}>
           <h2 style={{color : 'white'}}>About</h2>
+          </div>
+          <div className={styles.box}>
+          <div className={styles.inner}>
+          <h2>Movie 1</h2>
+          <p>This is a nice movie from the 90's with bruce willis.Check it out on netflix</p>
+
+          </div>
+          </div>
         </div>
     );
   }
--- src/components/engine/engine.js
+++ src/components/engine/engine.js
@@ -4,9 +4,8 @@ import '../../App.css'
 class Engine extends Component {
     render () {
         return (
-            <div  styles={{ backgroundImage: `url('https://i.imgur.com/TBSSmf6.jpg')` }}>
       <h1>This is red car</h1>
-    </div>
+
         )
     }
 }
