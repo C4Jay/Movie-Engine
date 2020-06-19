439123978ed852c7eee690707180c11d0239ccbc
C4Jay <choolakejay@gmail.com>
2020-06-19 11:06:09 +0530

    movies

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,66 @@
 import React, { Component } from 'react';
 import styles from './About.module.css'
+import axios from 'axios';
+
+var moviesresponse ={}
 
 class About extends Component {
+  state = {
+    movie: {},
+    Title: '',
+    Plot: ''
+  }
+
+  componentDidMount () {
+    axios.get('http://www.omdbapi.com/?apikey=6874092e&i=tt1285016')
+    .then(response => {
+      console.log(response.data)
+      console.log(response.data.Title)
+      console.log(response.data.Plot)
+      /* moviesresponse = response.data
+      console.log(moviesresponse) */
+      this.setState({
+        movie: response.data,
+        Title: response.data.Title,
+        Plot: response.data.Plot
+      })
+    })
+  }
   render() {
+    /* let list;
+    if(this.state.movies) {
+      list = this.state.movies.map(movie => {
+        return <div className={styles.box}>
+        <div className={styles.inner}>
+        <h2>{movie.Title}</h2>
+        <p>{movie.Plot}</p>
+  
+        </div>
+        </div>
+      })
+    } */
+    
+
     return (
         <div className={styles.main}>
           <div className={styles.title}>
-          <h2 style={{color : 'white'}}>About</h2>
+          <h1 style={{color : 'white'}}>About</h1>
           </div>
-          <div className={styles.box}>
-          <div className={styles.inner}>
-          <h2>Movie 1</h2>
-          <p>This is a nice movie from the 90's with bruce willis.Check it out on netflix</p>
-
+          {/* {list} */}
+         {/*  <div className={styles.box}>
+        <div className={styles.inner}>
+        <h2>{this.state.Title}</h2>
+        <p>{this.state.Plot}</p>
+  
+        </div>
+        </div> */}
+        <div className={styles.box2}>
+        <div className={styles.inner2}>
+          <h2>{this.state.Title}</h2>
+          <p>{this.state.Plot}</p>
           </div>
-          </div>
+        </div>
+          
         </div>
     );
   }
